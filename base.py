105b5d23764eb16550b3d3f2b3672c95de56b5f9
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-21 22:07:47 +0900

    [add]関数追加

--- base.py
+++ base.py
@@ -11,6 +11,27 @@ from contextlib import contextmanager
 import pandas as pd
 
 
+from spica.utils import timer
+
+
+def get_arguments(description):
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--force', '-f', action='store_true', help='Overwrite existing files')
+    return parser.parse_args()
+
+
+def get_features(namespace):
+    for k, v in namespace.items():
+        if inspect.isclass(v) and issubclass(v, Feature) and not inspect.isabstract(v):
+            yield v()
+
+
+def generate_features(namespace, overwrite):
+    for f in get_features(namespace):
+        if f.train_path.exists() and f.test_path.exists() and not overwrite:
+            print(f.name, 'was skipped')
+        else:
+            f.run().save()
 @contextmanager
 def timer(name):
     t0 = time.time()
