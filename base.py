966b167f0eed39610653462b4d5259f87e31ec24
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-21 22:03:14 +0900

    [add]base.py追加,特徴量作成のためのクラスが記述

---
+++ base.py
@@ -0,0 +1,49 @@
+#This code written by amaotone
+#https://amalog.hateblo.jp/entry/kaggle-feature-management
+
+
+import re
+import time
+from abc import ABCMeta, abstractmethod
+from pathlib import Path
+from contextlib import contextmanager
+
+import pandas as pd
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    print(f'[{name}] start')
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
+
+
+class Feature(metaclass=ABCMeta):
+    prefix = ''
+    suffix = ''
+    dir = '.'
+    
+    def __init__(self):
+        self.name = self.__class__.__name__
+        self.train = pd.DataFrame()
+        self.test = pd.DataFrame()
+        self.train_path = Path(self.dir) / f'{self.name}_train.ftr'
+        self.test_path = Path(self.dir) / f'{self.name}_test.ftr'
+    
+    def run(self):
+        with timer(self.name):
+            self.create_features()
+            prefix = self.prefix + '_' if self.prefix else ''
+            suffix = '_' + self.suffix if self.suffix else ''
+            self.train.columns = prefix + self.train.columns + suffix
+            self.test.columns = prefix + self.test.columns + suffix
+        return self
+    
+    @abstractmethod
+    def create_features(self):
+        raise NotImplementedError
+    
+    def save(self):
+        self.train.to_feather(str(self.train_path))
+        self.test.to_feather(str(self.test_path))
