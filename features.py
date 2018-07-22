171b463306370b7fe8b8e9bc8ae53025a9b826dc
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-22 11:23:23 +0900

    [add]statics,randomprojection追加

--- features.py
+++ features.py
@@ -1,6 +1,10 @@
 from base import Feature, get_arguments, generate_features
-import pandas as pd
 
+import pandas as pd
+import numpy as np 
+from scipy.stats import skew
+
+from sklearn import random_projection
 
 class select_features(Feature):
     def create_features(self):
@@ -17,10 +21,53 @@ class select_features(Feature):
         self.train = train[select]
         self.test = test[select]
 
+class statics(Feature):
+    def create_features(self):
+        
+        train_zeros = pd.DataFrame({'Percent_zero': ((train.values) == 0).mean(axis=0),
+                                'Column': train.columns})
+    
+        high_vol_columns = train_zeros['Column'][train_zeros['Percent_zero'] < 0.70].values
+        low_vol_columns = train_zeros['Column'][train_zeros['Percent_zero'] >= 0.70].values
+        
+        #train=train.replaceにするとtrainがローカル変数になってしまう
+        tmp_train = train.replace({0:np.nan})
+        tmp_test = test.replace({0:np.nan})
+
+        cluster_sets = {"low":low_vol_columns, "high":high_vol_columns}
+        for cluster_key in cluster_sets:
+            for df,self_df in [(tmp_train,self.train),(tmp_test,self.test)]:
+                self_df["count_not0_"+cluster_key] = df[cluster_sets[cluster_key]].count(axis=1)
+                self_df["sum_"+cluster_key] = df[cluster_sets[cluster_key]].sum(axis=1)
+                self_df["var_"+cluster_key] = df[cluster_sets[cluster_key]].var(axis=1)
+                self_df["median_"+cluster_key] = df[cluster_sets[cluster_key]].median(axis=1)
+                self_df["mean_"+cluster_key] = df[cluster_sets[cluster_key]].mean(axis=1)
+                self_df["std_"+cluster_key] = df[cluster_sets[cluster_key]].std(axis=1)
+                self_df["max_"+cluster_key] = df[cluster_sets[cluster_key]].max(axis=1)
+                self_df["min_"+cluster_key] = df[cluster_sets[cluster_key]].min(axis=1)
+                self_df["skew_"+cluster_key] = df[cluster_sets[cluster_key]].skew(axis=1)
+                self_df["kurtosis_"+cluster_key] = df[cluster_sets[cluster_key]].kurtosis(axis=1)
+
+        del(tmp_train)
+        del(tmp_test)
+
+class RandomProjection(Feature):
+    def create_features(self):
+        n_com = 100
+        transformer = random_projection.SparseRandomProjection(n_components = n_com)
+
+        self.train = pd.DataFrame(transformer.fit_transform(train))
+        self.test = pd.DataFrame(transformer.transform(test))
+        
+        columns = ["RandomProjection{}".format(i) for i in range(n_com)]
+        self.train.columns = columns
+        self.test.columns = columns
+
+
 if __name__ == '__main__':
     args = get_arguments()
 
-    train = pd.read_csv('input/train.csv')
-    test = pd.read_csv('input/test.csv')
+    train = pd.read_csv('input/train.csv').drop(["ID","target"],axis=1)
+    test = pd.read_csv('input/test.csv').drop("ID",axis=1)
 
-    select_features().run().save()
+    
