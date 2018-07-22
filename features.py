c192c198d322ccdab277ed72bc24a0ddc9d3bc37
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-22 16:20:28 +0900

    [fix]バグを修正

--- features.py
+++ features.py
@@ -70,4 +70,5 @@ if __name__ == '__main__':
     train = pd.read_csv('input/train.csv').drop(["ID","target"],axis=1)
     test = pd.read_csv('input/test.csv').drop("ID",axis=1)
 
-    
+    generate_features(globals(), args.force)
+
